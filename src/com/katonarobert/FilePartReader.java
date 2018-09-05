7a281a398cf4e81f5216263754343feddd225e66
robertkatona <30589040+robertkatona@users.noreply.github.com>
2018-09-05 12:41:17 +0200

    init enviroment

---
+++ src/com/katonarobert/FilePartReader.java
@@ -0,0 +1,4 @@
+package com.katonarobert;
+
+public class FilePartReader {
+}
---
+++ src/com/katonarobert/Main.java
@@ -0,0 +1,8 @@
+package com.katonarobert;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
