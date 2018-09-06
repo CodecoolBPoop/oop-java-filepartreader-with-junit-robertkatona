638561b265e6f83d83a49e4e84623da4a950edd3
robertkatona <30589040+robertkatona@users.noreply.github.com>
2018-09-06 14:40:44 +0200

    FileWordAnalzyer can create an Arraylist from a given string between the two given line number

--- src/com/katonarobert/FileWordAnalyzer.java
+++ src/com/katonarobert/FileWordAnalyzer.java
@@ -1,4 +1,41 @@
 package com.katonarobert;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+
 public class FileWordAnalyzer {
+
+    FilePartReader filePartReader;
+    String filePath;
+
+    public FileWordAnalyzer(FilePartReader filePartReader, String filePath) {
+        this.filePartReader = filePartReader;
+        this.filePath = filePath;
+    }
+
+    public ArrayList wordsByABC(){
+        ArrayList arrayListFromString = new ArrayList();
+        String stringToArrayList = filePartReader.readLines(filePath,1,2);
+        try {
+            StringReader reader = new StringReader(stringToArrayList);
+            BufferedReader br = new BufferedReader(reader);
+            String line;
+            StringBuffer stringBuffer = new StringBuffer();
+            while ((line = br.readLine()) != null) {
+                    arrayListFromString.add(line);
+
+            }return arrayListFromString;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return  new ArrayList(){};
+        }
+    }
+
+    //public ArrayList wordsContainingSubString(String subString){}
+
+    //public ArrayList wordsArePalindrome(){}
+
 }
--- src/com/katonarobert/Main.java
+++ src/com/katonarobert/Main.java
@@ -1,11 +1,15 @@
 package com.katonarobert;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
         FilePartReader fpr = new FilePartReader();
         String exampleString = fpr.read("/Users/katonarobert/IdeaProjects/oop-java-filepartreader-with-junit-robertkatona/src/example.txt");
         String exampleStringFromLineToLine = fpr.readLines("/Users/katonarobert/IdeaProjects/oop-java-filepartreader-with-junit-robertkatona/src/example.txt", 0,7);
-        System.out.println(exampleStringFromLineToLine);
+        //System.out.println(exampleStringFromLineToLine);
+        FileWordAnalyzer fwa = new FileWordAnalyzer(fpr,"/Users/katonarobert/IdeaProjects/oop-java-filepartreader-with-junit-robertkatona/src/example.txt");
+        ArrayList<String> exampleArrayList = fwa.wordsByABC();
     }
 }
